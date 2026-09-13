bce8e540512a6c74c2b67f6453a6bfb3b6b36643
kcstarlight7 <fiona112.chuang@gmail.com>
2026-09-13 17:26:58 -0600

    complete all but type casting question

--- src/UniquePtr.h
+++ src/UniquePtr.h
@@ -1,6 +1,83 @@
 #ifndef UNIQUE_PTR_H
 #define UNIQUE_PTR_H
+#include <utility>
 
 // Your implementation here
+template <typename T>
+class UniquePtr {
+    template <typename U>
+    friend class UniquePtr;
+public: 
+	UniquePtr(T* p = nullptr) : mPtr{p} {}
+    ~UniquePtr() {
+        if (mPtr != nullptr) {
+            delete mPtr;
+        }
+	}
+    UniquePtr(UniquePtr&& other) { 
+        mPtr = other.mPtr;
+        other.mPtr = nullptr;
+    }
+    UniquePtr& operator=(UniquePtr&& other) { 
+        if (this != &other) {
+            delete mPtr;
+            mPtr = other.mPtr;
+            other.mPtr = nullptr;
+        }
+        return *this;
+    }
+    template <typename U>
+    UniquePtr(UniquePtr<U>&& other) {
+        mPtr = other.mPtr;
+        other.mPtr = nullptr;
+    }
+    T& operator*() { 
+        return *mPtr;
+    }
+    T* operator->() { 
+        return mPtr;
+    }
+    T* get() { 
+        return mPtr;
+    }
+    bool operator==(const UniquePtr<T>& other) {
+        if (mPtr == other.mPtr) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    T* release() { 
+        T* res = mPtr;
+        delete mPtr;
+        return res;
+    }
+    void reset(T* newPtr = nullptr) {
+        T* prevptr = mPtr;
+        mPtr = newPtr;
+        if (prevptr != nullptr) {
+            delete prevptr;
+        }
+    }
+    void swap(UniquePtr<T>& other) { 
+        std::swap(*this, other);
+    }
+    operator bool() {
+        if (mPtr != nullptr) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+private: 
+    T* mPtr;
+};
+
+template <typename T, typename... Args>
+UniquePtr<T> makeUnique(Args&&... args) {
+    return UniquePtr<T>(new T(std::forward<Args>(args)...));
+}
+
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,13 @@
 #include "UniquePtr.h"
+#include <utility>
+#include <iostream>
 
 int main() {
-    // UniquePtr<int> ptr(new int(5));
+    UniquePtr<int> ptr(new int(5));
     // they test all methods here...
+    //UniquePtr<float> ptr2(std::move(ptr));
+    //std::cout << ptr.operator bool() << std::endl;
+    UniquePtr<int> uniquePtr1 = makeUnique<int>(5);
+    auto uniquePtr2 = makeUnique<int>(5);
     return 0;
 }
